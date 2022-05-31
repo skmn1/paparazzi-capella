06406020c4a7e3f62e86c734fe3a0a6ee0a9c1f0
soulimane <soulimane.kamni@ensma.fr>
2022-05-31 15:32:11 +0200

    nested function reimplemented using recursion - works with bugs

--- src/main/java/CPPXMLTransformer.java
+++ src/main/java/CPPXMLTransformer.java
@@ -1,8 +1,10 @@
 import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map.Entry;
 import java.util.Scanner;
+import java.util.Set;
 
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
@@ -12,11 +14,11 @@ import businessStructure.AADLFunction;
 import businessStructure.AADLThread;
 import helpers.FileHelper;
 
-public class CPPXMLTransformer
-{
+public class CPPXMLTransformer {
 
 	public static HashMap<String,String> functionFileMap = new HashMap<String,String>();
 	public static HashMap<String,Integer> fileDecompositionMap = new HashMap<String,Integer>();
+	public static HashMap<String,AADLFunction> functionSet = new HashMap<String,AADLFunction>();
 
 
 	public static void listFilesForFolder(final File folder) {
@@ -29,8 +31,6 @@ public class CPPXMLTransformer
 			}
 		}
 	}
-
-
 
 	public static void main(String[] args) throws Exception {
 
@@ -83,6 +83,7 @@ public class CPPXMLTransformer
 		parser.addParseListener(listener);
 
 		ParseTree tree = parser.translationUnit();
+		functionSet = listener.functionSet;
 
 		String XMLTags = getXMLfromThreadDataStructure(listener.threadSet);
 
@@ -98,31 +99,11 @@ public class CPPXMLTransformer
 		String str = "";
 		final String functionTabs = "\t";
 
-		Integer functionDecompositionLevel;
 		for (Entry<String, AADLThread>  threadEntry : threadSet.entrySet()) {
-			str += "<AADLThreadSet xsi:type=\"deployment:AADLThread\" id=\"_T_SWgNA7Eey3zKUaIaFcdg\"\r\n"
-					+ "                name=\"" + threadEntry.getKey() + "\" timeBudgetUnit=\"ms\">\r\n";
-			for (Entry<String, AADLFunction> ThreadFunctionSet : threadEntry.getValue().getThreadFunctionSet().entrySet()) {
-				functionDecompositionLevel = fileDecompositionMap.get(functionFileMap.get(ThreadFunctionSet.getKey()));
+			str += injectFunction(threadEntry);
 
-				if(ThreadFunctionSet.getValue().getSubFunctionSet().isEmpty() || functionDecompositionLevel == 0) {
-					str += functionTabs + "<ownedExtensions xsi:type=\"deployment:AADLFunction\" id=\"baaec95a-deed-4d91-9998-c10e6aec4ad2\"\r\n"
-							+ functionTabs + functionTabs +"name=\"" + ThreadFunctionSet.getKey() + " " + functionDecompositionLevel + " (" + functionFileMap.get(ThreadFunctionSet.getKey()) + ")\"/>\r\n";
-				}
-				else {
-					str += functionTabs + "<ownedExtensions xsi:type=\"deployment:AADLFunction\" id=\"baaec95a-deed-4d91-9998-c10e6aec4ad2\"\r\n"
-							+ functionTabs + functionTabs +"name=\"" + ThreadFunctionSet.getKey() + " " + functionDecompositionLevel + " (" + functionFileMap.get(ThreadFunctionSet.getKey()) + ")\">\r\n";
-					for (String subFunctionName : ThreadFunctionSet.getValue().getSubFunctionSet()) {
-
-						str += functionTabs + "<ownedExtensions xsi:type=\"deployment:AADLFunction\" id=\"baaec95a-deed-4d91-9998-c10e6aec4ad2\"\r\n"
-								+ functionTabs + functionTabs +"name=\"" + subFunctionName + "\"/>\r\n";
-					}
-					str += "</ownedExtensions>\n";
-				}
-			}
-			str += "<header xsi:type=\"deployment:AADLThreadHeader\" id=\"_T_SWgdA7Eey3zKUaIaFcdg\"/>\r\n"
-					+ "            </AADLThreadSet>\n";
 		}
+
 		return str;
 	}
 
@@ -140,7 +121,6 @@ public class CPPXMLTransformer
 
 		parser.addParseListener(listener);
 
-
 		ParseTree tree = parser.translationUnit();
 	}
 
@@ -155,4 +135,76 @@ public class CPPXMLTransformer
 			System.out.println("function name : " + entry.getKey() +" ======> " + entry.getValue()+"\n\n" );
 		}
 	}
+
+	public static String injectFunction (Entry<String, AADLThread>  threadEntry) {
+
+		String str = "";
+
+		//		 thread creation
+		str += "<AADLThreadSet xsi:type=\"deployment:AADLThread\" id=\"_T_SWgNA7Eey3zKUaIaFcdg\"\r\n"
+				+ "                name=\"" + threadEntry.getKey() + "\" timeBudgetUnit=\"ms\">\r\n";
+		String functionTabs = "\t";
+
+		//		thread functions creation
+		for (Entry<String, AADLFunction> ThreadFunctionSet : threadEntry.getValue().getThreadFunctionSet().entrySet()) {
+			if(ThreadFunctionSet.getValue().getSubFunctionSet().isEmpty())
+
+				//				if the function doesn't contain any subfunction then we create autoclosed xml thread function
+				str += functionTabs + "<ownedExtensions xsi:type=\"deployment:AADLFunction\" id=\"baaec95a-deed-4d91-9998-c10e6aec4ad2\"\r\n"
+						+ functionTabs + functionTabs +"name=\"" + ThreadFunctionSet.getKey() + "::" + functionFileMap.get(ThreadFunctionSet.getKey()) + "\"/>\r\n";
+			else {
+				//				if the thread function has some subfunctions
+				//				we create the function and then parcour the functions by calling the injectsubfunction method
+				str += functionTabs + "<ownedExtensions xsi:type=\"deployment:AADLFunction\" id=\"baaec95a-deed-4d91-9998-c10e6aec4ad2\"\r\n"
+						+ functionTabs + functionTabs +"name=\"" + ThreadFunctionSet.getKey() + "::" + functionFileMap.get(ThreadFunctionSet.getKey()) + "\">\r\n";
+
+				// the injectsubfunction must be called here
+
+				for (String subFunctionName : ThreadFunctionSet.getValue().getSubFunctionSet()) {
+					str += injectSubFunction(subFunctionName);
+				}
+
+				str += "</ownedExtensions>\n";
+			}
+		}
+
+		str += "<header xsi:type=\"deployment:AADLThreadHeader\" id=\"_T_SWgdA7Eey3zKUaIaFcdg\"/>\r\n"
+				+ "            </AADLThreadSet>\n";
+
+		return str;
+
+	}
+
+	public static String  injectSubFunction (String subFunctionName) {
+		String str = "";
+		String functionTabs = "\t";
+
+		System.err.println("subfunction name : " + subFunctionName);
+		System.err.println(functionSet);
+		//		if(functionSet.containsKey(subFunctionName)){
+		if( functionSet.get(subFunctionName).getSubFunctionSet().isEmpty())
+			str += functionTabs  + "<ownedExtensions xsi:type=\"deployment:AADLFunction\" id=\"baaec95a-deed-4d91-9998-c10e6aec4ad2\"\r\n"
+					+ functionTabs + functionTabs +"name=\"" + subFunctionName + "\"/>\r\n";
+		else {
+
+			str += functionTabs + "<ownedExtensions xsi:type=\"deployment:AADLFunction\" id=\"baaec95a-deed-4d91-9998-c10e6aec4ad2\"\r\n"
+					+ functionTabs + functionTabs +"name=\"" + subFunctionName + "\">\r\n";
+
+			for (String subSubFunctionName : functionSet.get(subFunctionName).getSubFunctionSet()) {
+				str += injectSubFunction(subSubFunctionName);
+			}
+			str += "</ownedExtensions>\n";
+		}
+		//		}
+		return str;
+	}
+
+	public void injectX (String x) {
+
+	}
+
+
+
+
+
 }
